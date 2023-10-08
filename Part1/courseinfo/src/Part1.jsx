f819c82f59fd6eba3bb84513294ec1c8c833fceb
RebekahEloise <rebekaheloisemiller@gmail.com>
2023-10-07 22:00:37 -0500

    three components for content

---
+++ Part1/courseinfo/src/Part1.jsx
@@ -0,0 +1,15 @@
+const Part1 = () =>{
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+
+
+    return (
+        <div>
+      <p>
+        {part1} {exercises1}
+      </p>
+        </div>
+    )
+}
+
+export default Part1
---
+++ Part1/courseinfo/src/Part3.jsx
@@ -0,0 +1,15 @@
+const Part3 = () =>{
+    const part3 = 'State of a component'
+    const exercises3 = 14
+
+
+    return (
+        <div>
+      <p>
+        {part3} {exercises3}
+      </p>
+        </div>
+    )
+}
+
+export default Part3
